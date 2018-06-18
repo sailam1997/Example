9fde090db5aecd21d2e5971dce0e3d9a62a742c5
test <demoesting8888@gmail.com>
2018-06-18 09:17:17 +0700

    hello

---
+++ HinhTron.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class HinhTron {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        float D;
+        D= scanner.nextFloat();
+        double P = (D*3.14);
+        System.out.println("Chu vi hinh tron la:"+P);
+        double S= ((D/2)*(D/2)*3.14);
+        System.out.println("Dien tich hinh tron la"+S);
+
+
+
+
+
+    }
+}
